bd4a1bf21a2c34e509615bfe157c72a6d9b4ba95
ttsantos <49454116+ttsantos@users.noreply.github.com>
2019-05-14 23:57:00 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=114zZ-ctg4fCM775Ll68fWhWJl43PXO5o'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-LxqtqOoZuevATwyv5oLpPjnB-POCroB'
 export_file_name = 'export.pkl'
 
 
