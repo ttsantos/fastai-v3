e4b311b4e22dec45d23a87947ebce2fc778b6196
ttsantos <49454116+ttsantos@users.noreply.github.com>
2019-05-15 01:58:19 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 #export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&confirm=I0mM&id=1-LxqtqOoZuevATwyv5oLpPjnB-POCroB'
+export_file_url = 'https://drive.google.com/uc?export=download&confirm=i_M8&id=1-LxqtqOoZuevATwyv5oLpPjnB-POCroB'
 export_file_name = 'export.pkl'
 
 classes = ['Calçada',
