8e872487ae549ef5cc6527d6c7d5a1dae4d70244
ttsantos <49454116+ttsantos@users.noreply.github.com>
2019-05-15 02:10:40 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,8 +8,8 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-#export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&confirm=i_M8&id=1-LxqtqOoZuevATwyv5oLpPjnB-POCroB'
+export_file_url = 'https://www.dropbox.com/s/regbrphkv7rfvpa/export.pkl?dl=1'
+#export_file_url = 'https://drive.google.com/uc?export=download&confirm=i_M8&id=1-LxqtqOoZuevATwyv5oLpPjnB-POCroB'
 export_file_name = 'export.pkl'
 
 classes = ['Calçada',
